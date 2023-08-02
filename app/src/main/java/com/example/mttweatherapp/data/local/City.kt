30377cbf928876e98f46cb2ed9032fbb38b2b1cf
jnmii <mbuyambajoseph@gmail.com>
2023-08-02 15:50:13 -0400

    inital commit, adjusted packages

--- app/src/main/java/com/example/mttweatherapp/data/local/City.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mttweatherapp.data.local
-
-class City {
-}
---
+++ app/src/main/java/com/example/mttweatherapp/ui/forcastcomponent.kt
@@ -0,0 +1,15 @@
+package com.example.mttweatherapp.ui
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backgroundColor:Color,
+    modifier: Modifier =Modifier
+){
+    Card {
+        background
+    }
+}
